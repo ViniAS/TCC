95657bd317e74bb995f08d91373ba68ea38636ec
Vinicius Antunes <vinicius.antunes@proton.me>
2025-11-12 22:16:10 -0300

    add county site data

---
+++ src/county_site_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+if __name__ == "__main__":
+    df = pd.read_parquet('data/agg_data/graph.parquet')
+    
+    df['HOSPxDIST'] = df['HOSPITALIZACOES'] * df['DISTANCE']
+    distdf = df.groupby(['MUNIC_RES','ANO_CMPT','DIAG_PRINC']).agg({'HOSPITALIZACOES':'sum','HOSPxDIST':'sum'})
+    distdf['DISTANCE'] = distdf['HOSPxDIST'] / distdf['HOSPITALIZACOES']
+    distdf = distdf.drop('HOSPxDIST',axis =1)
+    distdf = distdf['DISTANCE'].round(3)
+    
+    distdf.to_csv('docs/static/data/counties.csv')
